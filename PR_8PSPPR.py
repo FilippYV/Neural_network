edd8717aca7ada1deef1a0fcf79f2c975dea4181
Filisss_911 <102216221+FilippYV@users.noreply.github.com>
2022-12-22 11:15:40 +0300

    Final

--- PR_8PSPPR.py
+++ PR_8PSPPR.py
@@ -1,9 +1,5 @@
 import random
 
-import numpy as np
-
-
-# 3 на 5
 
 class Neural:
     def __init__(self, inputs, weight, step, answers, last_error, epochs):
@@ -16,27 +12,32 @@ class Neural:
         self.t = 0
 
     def train(self):
-        for eph in range(self.epochs):
-            for count in range(len(self.inputs)):
+        # for eph in range(self.epochs):
+        for i in range(epochs):
+            count = 0
+            while count != len(self.inputs):
                 self.summ_to_active = 0
                 for i in range(len(self.inputs[count])):
                     self.summ_to_active += self.inputs[count][i] * self.weight[i]
                 self.summ_to_active -= self.t
-                if self.summ_to_active >= 1:
+                if self.summ_to_active > 0:
                     self.summ_to_active = 1
                 else:
+                    self.summ_to_active = -1
+                if self.summ_to_active != self.answers[count]:
                     for i in range(len(self.weight)):
-                        len(self.weight)
-                        print(self.step * self.inputs[count][i] * self.answers[count])
-                        print('----------------')
-                        self.weight[i] = round(self.weight[i] + self.step * self.inputs[count][i] * self.answers[count],
+                        self.weight[i] = round(self.weight[i] + self.inputs[count][i] * self.answers[count],
                                                5)
+                        # print(self.weight[i], i)
                     self.t = self.t - self.answers[count]
-            print(f'Эпоха №{eph} -', self.weight)
+                    count = 0
+                else:
+                    count += 1
+        # print(f'Эпоха №{eph} -', self.weight)
 
     def start(self, new_value):
         self.train()
-        print('Веса -', self.weight)
+        print('\nВеса -', self.weight)
         final_answer = 0
         for i in range(len(new_value)):
             final_answer += new_value[i] * self.weight[i]
@@ -48,37 +49,42 @@ class Neural:
         else:
             print('Ответ:', -1)
 
-    def start_no_train(self, new_value):
-        self.train()
-        print('Веса -', self.weight)
-        final_answer = 0
-        for i in range(len(new_value)):
-            final_answer += new_value[i] * self.weight[i]
-        final_answer -= self.t
-
-        if final_answer >= 0:
-            final_answer = 1
-            print('Ответ:', final_answer)
-        else:
-            print('Ответ:', -1)
-
 
 def random_weight(lens):
     mass_weigh = []
     for i in range(lens):
-        mass_weigh.append(1/lens)
+        mass_weigh.append(round(random.uniform(0, 1), 5))
     return mass_weigh
 
 
 if __name__ == '__main__':
     last_error = 0
     step = 0.01
-    epochs = 1000
-    mass_value = [[1, 1, 1, 1],
-                  [-1, -1, -1, -1]]
-    mass_answers = [1, -1]
-    brain = Neural(mass_value, random_weight(len(mass_value[0])), step, mass_answers, last_error, epochs)
-    brain.start([-1, -1, -1, 1]) # [temro_p_exist, cistem_coolers, cooler_coonect, cooler_dress, termopaste_o]
+    epochs = 1
+    mass_value = [[-1, -1, -1, -1],
+                  [-1, -1, -1, 1],
+                  [-1, -1, 1, -1],
+                  [-1, -1, 1, 1],
+                  [-1, 1, -1, -1],
+                  [-1, 1, -1, 1],
+                  [-1, 1, 1, -1],
+                  [-1, 1, 1, 1],
+                  [1, -1, -1, -1],
+                  [1, -1, -1, 1],
+                  [1, -1, 1, -1],
+                  [1, -1, 1, 1],
+                  [1, 1, -1, -1],
+                  [1, 1, -1, 1],
+                  [1, 1, 1, -1],
+                  [1, 1, 1, 1]]
+    mass_answers = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1]
+    brain = Neural(mass_value,
+                   random_weight(len(mass_value[0])),
+                   step,
+                   mass_answers,
+                   last_error,
+                   epochs)
+    brain.start([1, 1, 1, 1])  # [temro_p_exist, cistem_coolers, cooler_coonect, cooler_dress, termopaste_o]
     # brain = Neural(mass_value, [0.69662, 0.95457, 0.57743, 0.88189, 0.00117], step, mass_answers, last_error, epochs)
     # brain.start_no_train([-1, -1, 1, -1, 1])
     # -1.62392, 1.21987, 0.30287, 4.63544, 2.55954]
